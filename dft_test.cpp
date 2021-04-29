09088f78b335088ace5244336020ce7d5c8fc802
Ubuntu <ubuntu@ip-172-31-25-192.us-east-2.compute.internal>
2021-04-29 23:50:34 +0000

    debugging

--- dft_test.cpp
+++ dft_test.cpp
@@ -30,10 +30,11 @@ int main() {
         }
         std::cout << std::endl;
     }
+    std::cout << std::endl;
 
-    //dft_alloc(image2, dft_w, dft_h, dft_space2);
+    dft_alloc(image2, dft_w, dft_h, dft_space2);
     dft(dft_w, dft_h, dft_space1);
-    //dft(dft_w, dft_h, dft_space2);
+    dft(dft_w, dft_h, dft_space2);
     
     
     std::cout << "dft_space1 after dft: " << std::endl;
@@ -43,17 +44,22 @@ int main() {
         }
         std::cout << std::endl;
     }
+    std::cout << std::endl;
     
     
     
-    
-    
-    //dft_multiply(dft_w, dft_h, dft_space1, dft_space2);
-    //dft(dft_w, dft_h, dft_space1, true);
+    std::cout << std::endl;
+    dft_multiply(dft_w, dft_h, dft_space1, dft_space2);
+    dft(dft_w, dft_h, dft_space1, true);
      
-
-
-
+    std::cout << "dft_space1 after multiplication: " << std::endl;
+    for (int y = 0, index = 0; y < dft_h; ++ y) {
+        for (int x = 0; x < dft_w; ++ x, ++ index) {
+            std::cout << dft_space1[index].b << " ";
+        }
+        std::cout << std::endl;
+    }
+    std::cout << std::endl;
 
 
 
@@ -117,6 +123,20 @@ int main() {
     DFT::dft_multiply(dft_w, dft_h, fftw_space1Out, fftw_space2Out, fftw_resultIn);
     
     DFT::dft(fftw_resultIn, fftw_resultOut, dft_w, dft_h);
+
+
+    std::cout << std::endl;
+    std::cout << "fftw_resultOut normalized：" << std::endl;
+    for (int i = 0; i < dft_h; ++ i) {
+        for (int j = 0; j < dft_w; ++ j) {
+            std::cout << "(" << fftw_resultOut[i * dft_w + j][REAL] / (dft_h * dft_w) << ","
+                    << fftw_resultOut[i * dft_w + j][IMAG] / (dft_h * dft_w) << ") ";
+        }
+        std::cout << std::endl;
+    }
+
+
+
 
     // Show result
 
